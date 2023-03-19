cccf89fdbce5298d8214db65083ab541fa82d0d9
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-19 19:34:50 +0300

    nn-project_7 ver. 1.1 (Создал админ)

--- stock_trainer/main/models.py
+++ stock_trainer/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Portfolio(models.Model):
+    title = models.CharField('Название актива', max_length=100)
+    num = models.IntegerField('Кол-во')
+    price = models.IntegerField('Цена')
+    month = models.IntegerField('Месяц')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Актив'
+        verbose_name_plural = 'Активы'
+
